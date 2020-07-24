81eea041d4289a3ebcd92ebea5100970f1b42e42
Ruchir Dixit <ruchirtd96@gmail.com>
2020-07-24 12:48:06 +0530

    Update Hello.java
    Creating conflict

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Hello {
 	public static void main(String[] args) {
 		while(true) {
-			System.out.println("Hello World");
-			System.out.println("Changes done!");
+			System.out.println("Hello Mars");
+			System.out.println("No Channges here bro! conflict!");
 			try{
 					Thread.sleep(60000);
 				}catch(Exception ex){
