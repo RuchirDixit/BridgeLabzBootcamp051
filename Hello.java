7504568d55f1da5a4a749eea9cf98b3fedbfc136
Ruchir Dixit <ruchirtd96@gmail.com>
2020-07-24 12:51:22 +0530

    Going to have conflict

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Hello {
 	public static void main(String[] args) {
 		while(true) {
-			System.out.println("Hello World");
-			System.out.println("Changes done!");
+			System.out.println("Hello Jupiter");
+			System.out.println("Changes undone!");
 			try{
 					Thread.sleep(60000);
 				}catch(Exception ex){
