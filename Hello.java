653b4bdbc1181a51ba4fe9724ad7b01dbc1eb571
Ruchir Dixit <ruchirtd96@gmail.com>
2020-07-24 12:36:06 +0530

    Update Hello.java
    Added new System.out.println statement for changes done

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 	public static void main(String[] args) {
 		while(true) {
 			System.out.println("Hello World");
+			System.out.println("Changes done!");
 			try{
 					Thread.sleep(60000);
 				}catch(Exception ex){
