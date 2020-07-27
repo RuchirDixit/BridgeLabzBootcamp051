14e2fae020187d09f3049230837dc0481fa7cf4b
Ruchir Dixit <ruchirtd96@gmail.com>
2020-07-27 11:31:44 +0530

    Master branch update

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 	public static void main(String[] args) {
 		while(true) {
 			System.out.println("Hello Mars");
-			System.out.println("No Channges here bro! conflict!");
+			System.out.println("This is master branch");
 			try{
 					Thread.sleep(60000);
 				}catch(Exception ex){
